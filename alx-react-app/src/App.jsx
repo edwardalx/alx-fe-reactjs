67232ae13eded25434db2c8c3b4d9bcdfb9f0d77
edwardalx <eobkwaku@gmail.com>
2025-08-27 21:49:42 +0100

    Ex3

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,6 +19,9 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <Header />
+        <MainContent></MainContent>
+        <Footer></Footer>
       </div>
       <h1>Vite + React</h1>
       <WelcomeMessage />
---
+++ alx-react-app/src/components/Header.jsx
@@ -0,0 +1,5 @@
+export default function Footer(){
+return(<header>
+  <h1>My Favorite Cities</h1>
+</header>);
+}
