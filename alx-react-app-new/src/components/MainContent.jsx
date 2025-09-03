dd9ddfa7c0cd2798b80dbf279bf49d06d750586f
edwardalx <eobkwaku@gmail.com>
2025-09-03 21:31:50 +0100

    Week10 Ex1

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 export default function MainContent() {
 return(
     <main>
-  <p>I love to visit <span style={{color:"red"}} >New York</span> , <span style={{color:"blue"}}>Paris</span>, and <span style={{color:"green"}}>Tokyo</span>.</p>
+  <p style={{color:"red", fontWeight:"bold"}}>I love to visit New York, Paris, and Tokyo.</p>
 </main>
 );
 }
