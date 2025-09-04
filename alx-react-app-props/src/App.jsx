211ff62f08032f1138321713b951d988f67849a9
edwardalx <eobkwaku@gmail.com>
2025-09-04 23:01:50 +0100

    week10 ex3

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import WelcomeMessage from './components/WelcomeMessage'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import UserProfile from './components/UserProfile'
+import ProfilePage from './drillTestComponents/ProfilePage'
+import { UserContext } from './drillTestComponents/UserContext'
+
+function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+
+  return (
+    <>
+        <Header />
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+        <UserContext.Provider value={userData}><ProfilePage /></UserContext.Provider> 
+        <MainContent />
+        <Footer />
+        <WelcomeMessage />
+    </>
+  )
+}
+
+export default App
