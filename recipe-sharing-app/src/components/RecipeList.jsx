f870730d859839e58b0ccaaf0b597774c6527028
edwardalx <eobkwaku@gmail.com>
2025-09-13 00:05:52 +0100

    Week11_ex2

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,33 +1,40 @@
 // RecipeList component
-  import { useRecipeStore } from './recipeStore';
-  import RecipeDetails from './RecipeDetails';
-  import { useNavigate } from 'react-router-dom';
-  import SearchBar from './SearchBar';
+import { useRecipeStore } from "./recipeStore";
+import RecipeDetails from "./RecipeDetails";
+import { useNavigate } from "react-router-dom";
+import SearchBar from "./SearchBar";
 
-  const RecipeList = () => {
-    const recipes = useRecipeStore(state => state.recipes);
-    const filteredRecipes = useRecipeStore(state => state.filteredRecipes)
-    const navigate = useNavigate();
+const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
+  const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
+  // const navigate = useNavigate();
+  let finalRecipes;
+  filteredRecipes.length !== 0
+    ? (finalRecipes = filteredRecipes)
+    : (finalRecipes = recipes);
+  // const handleDetails = (id) => {
+  //   console.log("Details for:", recipe);
+  //   navigate(`/details/${id}`);
+  // };
 
-    const handleDetails = (id) => {
-      // console.log("Details for:", recipe);
-      navigate(`/details/${id}`)
-    }
-
-    return (
-      <div>
-        {recipes.map(recipe => (
-          <div key={recipe.id} >
-            <h3>{recipe.title}</h3>
-            <p>{recipe.description}</p>
-            <p style={{color:'red', cursor:'pointer'}} onClick={() => handleDetails(recipe.id)}>Click here fore detail</p>
-          </div>
-        ))}
-        <div>
-          <SearchBar />
+  return (
+    <div>
+      {finalRecipes.map((recipe) => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+          <p> 
+          <Link to={`/details/${id}`} style={{ color: "red", cursor: "pointer" }}>
+             Click here fore detail
+          </Link>
+          </p>
         </div>
+      ))}
+      <div>
+        <SearchBar />
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-  export default RecipeList
+export default RecipeList;
