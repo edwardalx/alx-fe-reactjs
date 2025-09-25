db139e1baffa3d2e08ea796b5ac4cd979afe8191
edwardalx <eobkwaku@gmail.com>
2025-09-25 21:27:04 +0100

    week12 ex3

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -5,7 +5,7 @@ function UserProfile() {
       {/* <img src={MyLogo} alt="Logo"  /> */}
       <img src="/vite.svg" alt="User" className="rounded-full  sm:w-24 sm:h-24 md:w-36 md:h-36"/>
       <h1 className="sm:text-lg md:text-xl text-blue-800 my-4" >John Doe</h1>
-      <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+      <p className="text-gray-600 sm: text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
 }
