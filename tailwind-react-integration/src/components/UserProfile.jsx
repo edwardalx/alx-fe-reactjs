5ce3c237e5aed6806b1492563bebea5d5361d3b1
edwardalx <eobkwaku@gmail.com>
2025-09-25 21:24:46 +0100

    week12 ex3

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 import MyLogo from "../assets/react.svg"
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 sm:p-4 md:p-8 sm:max-w-sm md:mx-auto my-20 rounded-lg shadow-lg">
+    <div className="user-profile bg-gray-100 sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
       {/* <img src={MyLogo} alt="Logo"  /> */}
       <img src="/vite.svg" alt="User" className="rounded-full  sm:w-24 sm:h-24 md:w-36 md:h-36"/>
       <h1 className="sm:text-lg md:text-xl text-blue-800 my-4" >John Doe</h1>
