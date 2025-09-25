82c169a6b600ef1610c8e22c6f6af49551cc0a8a
edwardalx <eobkwaku@gmail.com>
2025-09-25 22:48:02 +0100

    week13 ex4

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,11 +1,11 @@
 import MyLogo from "../assets/react.svg"
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="user-profile bg-gray-100 sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl hover:p-10">
       {/* <img src={MyLogo} alt="Logo"  /> */}
-      <img src="/vite.svg" alt="User" className="rounded-full  sm:w-24 sm:h-24 md:w-36 md:h-36"/>
+      <img src="/vite.svg" alt="User" className="rounded-full  sm:w-24 sm:h-24 md:w-36 md:h-36 hover:scale-110 transition-transform duration-300 ease-in-out"/>
       <h1 className="sm:text-lg md:text-xl text-blue-800 my-4" >John Doe</h1>
-      <p className="text-gray-600 sm: text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+      <p className="text-gray-600 sm: text-sm md:text-base hover:text-blue-500">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
 }
