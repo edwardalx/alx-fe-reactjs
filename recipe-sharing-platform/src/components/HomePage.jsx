98e8434094f6b56d016996ee0a271cc109de490a
edwardalx <eobkwaku@gmail.com>
2025-09-30 00:14:02 +0100

    Week14_ex2

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -11,12 +11,26 @@ export default function HomePage() {
 
   return (
     <div>
-      <div className="text-xl">HomePage</div>
-      {data.map((x) => (
-        <div key={x.id}>
-          <Card image={x.image} title={x.title} />
-        </div>
-      ))}
+      <div className="text-xl ">HomePage</div>
+      <div className="flex flex-wrap">
+        {data.map((x) => (
+          <div key={x.id}>
+            <div className="user-profile bg-gray-100 sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl hover:p-10 ">
+              <img
+                src={x.img}
+                alt="User"
+                className="rounded-full  sm:w-24 sm:h-24 md:w-36 md:h-36 hover:scale-110 transition-transform duration-300 ease-in-out"
+              />
+              <h1 className="sm:text-lg md:text-xl text-blue-800 my-4">
+                {x.title}
+              </h1>
+              <p className="text-gray-600 sm: text-sm md:text-base hover:text-blue-500">
+                {x.summary}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
