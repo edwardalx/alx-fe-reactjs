2f01f0049ac2f9fbf10be3e0f0b0b84bde3dd5a7
edwardalx <eobkwaku@gmail.com>
2025-09-30 23:52:28 +0100

    week14 ex3

--- recipe-sharing-platform/src/components/Card.jsx
+++ recipe-sharing-platform/src/components/Card.jsx
@@ -5,7 +5,7 @@ export default function Card({img,title,summary}) {
       <div className="user-profile bg-gray-100 sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl hover:p-10">
         <img
           src={img}
-          alt="User"
+          alt={"User"}
           className="rounded-full  sm:w-24 sm:h-24 md:w-36 md:h-36 hover:scale-110 transition-transform duration-300 ease-in-out"
         />
         <h1 className="sm:text-lg md:text-xl text-blue-800 my-4">{title}</h1>
--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import fetchJsonData from "../services/Data_Json";
 import Card from "./Card";
 import { useParams } from "react-router-dom";
+import configData from "../data.json";
 
 export default function RecipeDetail() {
   const { id } = useParams();
@@ -25,11 +26,11 @@ export default function RecipeDetail() {
   }, [id]);
   if (!data) {return <p>Recipe Not Found</p>}
   return (
-    <div>
-      RecipeDetail
+    <div className="flex justify-center flex-col gap-8">
+      <div>RecipeDetail</div>
       {loading && <p>Loading...</p>}
-      <div key={id}>
-        <Card img={data.image} title={data.title} summary={data.summary} />
+      <div key={id} className="bg-[rgba(123,178,224,0.3)] text-xl">
+        <Card img={data.image} title={data.ingredients} summary={data.instructions} />
       </div>
     </div>
   );
