e7e8a0819d92deccce12169467745a3b59e13250
edwardalx <eobkwaku@gmail.com>
2025-10-08 22:08:52 +0100

    Week14_ex1

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,22 +1,35 @@
 import React, { useState } from "react";
+import { object } from "yup";
 
 export default function RegistrationForm() {
-  const [error, setErrors] = useState([])
+  const [error, setErrors] = useState({});
   const [formData, setFormData] = useState({
     username: "",
     email: "",
     password: "",
   });
   const handleChange = (e) => {
-    const { username, value } = e.target;
+    const { name, value } = e.target;
     setFormData((prev) => ({ ...prev, [name]: value }));
   };
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!formData.email){setErrors("Please enter a username"||null)}    /*if (!email) */
-    if (!formData.username){setErrors("Please enter username"||null)}   /*if (!username) */
-    if (!formData.password){setErrors("Please enter password"||null)}   /*if (!password) */
-    console.log("Form Data: ", formData);
+    const newErrors = {};
+    if (!formData.email) {
+      newErrors.email = "Please enter a username";
+    } /*if (!email) */
+    if (!formData.username) {
+      newErrors.username = "Please enter username";
+    } /*if (!username) */
+    if (!formData.password) {
+      newErrors.password = "Please enter password";
+    } /*if (!password) */
+
+    setErrors(newErrors);
+
+    if (Object.values(newErrors).length === 0) {
+      console.log("Form Data: ", formData);
+    }
   };
   return (
     <div>
@@ -24,7 +37,6 @@ export default function RegistrationForm() {
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="username">Username</label>
-          <div>{error[1] && <p>{error[1]}</p>}</div>
           <input
             type="text"
             id="username"
@@ -32,6 +44,7 @@ export default function RegistrationForm() {
             value={formData.username} /* value={username} */
             onChange={handleChange}
           />
+          <div>{error.username && <p className="text-red-600">{error.username}</p>}</div>
         </div>
         <div>
           <label htmlFor="email">Email</label>
@@ -39,9 +52,10 @@ export default function RegistrationForm() {
             type="email"
             id="email"
             name="email"
-            value={formData.email}   /* value={email} */
+            value={formData.email} /* value={email} */
             onChange={handleChange}
           />
+          <div>{error.email && <p className="text-red-600">{error.email}</p>}</div>
         </div>
         <div>
           <label htmlFor="password">Password</label>
@@ -49,9 +63,10 @@ export default function RegistrationForm() {
             type="password"
             id="password"
             name="password"
-            value={formData.password}  /* value={password} */
+            value={formData.password} /* value={password} */
             onChange={handleChange}
           />
+          <div>{error.password && <p className="text-red-600">{error.password}</p>}</div>
         </div>
         <button type="submit">Register</button>
       </form>
