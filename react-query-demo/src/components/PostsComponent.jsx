079f4d8a54032ec6017d2a3e2c16b12b91861026
edwardalx <eobkwaku@gmail.com>
2025-10-09 00:44:36 +0100

    Ex2

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,28 +1,37 @@
-import { QueryClient, QueryClientProvider, useQuery } from "@tanstack/react-query";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
 
-import React from 'react';
+import React from "react";
 import axios from "axios";
 
 export default function PostsComponent() {
-    const queryClent = useQueryClient()
-    const {data, isLoading, error, refetch, isFetching} = useQuery("posts", ()=>
-     axios("https://jsonplaceholder.typicode.com/posts").then((res)=>res.data).then(body=>console.log("data : ", body))
-);
-enabled:false
-const cachedData = queryClent.getQueryData("posts")
-console.log("Cached data: ", cachedData)
+    const fetchPost = ()=>{
+         axios("https://jsonplaceholder.typicode.com/posts")
+        .then((res) => res.data)
+        .then((body) => console.log("data : ", body))
+    }
+  const queryClent = useQueryClient();
+  const { data, isLoading, error, refetch, isFetching } = useQuery(
+    "posts",
+    () => fetchPost
+  );
+  enabled: false;
+  const cachedData = queryClent.getQueryData("posts");
+  console.log("Cached data: ", cachedData);
   return (
     <>
-    <div>PostsComponent</div>
-    <button onClick={()=>refetch()} disabled={isFetching}>
-        {isFetching? "Loading...":"Fetch Pasts"}
-    </button>
-    {isLoading&&<p>Loading...</p>}
-    {error&&<p className="text-red-600">Error loading the data</p>}
-    {data.map((x)=>
-    <div key={x.id}>Title: {x.title}</div>
-    )}
+      <div>PostsComponent</div>
+      <button onClick={() => refetch()} disabled={isFetching}>
+        {isFetching ? "Loading..." : "Fetch Pasts"}
+      </button>
+      {isLoading && <p>Loading...</p>}
+      {error && <p className="text-red-600">Error loading the data</p>} {/* isError*/}
+      {data.map((x) => (
+        <div key={x.id}>Title: {x.title}</div>
+      ))}
     </>
-    
-  )
+  );
 }
